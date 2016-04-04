a1f340dea6ac06661959918c3d10ce19c35a546f
David Graves <davidgraves831@gmail.com>
2016-04-04 14:06:40 -0600

    Added Username

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   resources :posts
   devise_for :users
   root "posts#index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228235306) do
+ActiveRecord::Schema.define(version: 20160404194755) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "description"
@@ -39,9 +39,11 @@ ActiveRecord::Schema.define(version: 20160228235306) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
